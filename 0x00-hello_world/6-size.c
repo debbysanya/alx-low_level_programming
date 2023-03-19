3dfca0ec6645e52be159eeabbe81c4597a67b0f6
Tolulope <debbyonasanya@gmail.com>
2023-03-18 23:43:26 -0700

    sizeof

---
+++ 0x00-hello_world/6-size.c
@@ -0,0 +1,44 @@
+#include <stdio.h>
+/**
+ * main - This is the main function
+ *
+ * Return: always (0)
+ */
+int main(void)
+{
+	printf("size of a char: %lu byte(s)". sizeof(char));
+	primtf("size of an int: %lu byte(s)". sizeof(int));
+	printf("size of a long int: %lu byte(s)". sizeof(long int));
+	printf("size of a long long int: %lu byte(s)". sizeof(long long int);
+	printf("size of a float: %lu byte(s)". size of(float));
+	return (0);
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
