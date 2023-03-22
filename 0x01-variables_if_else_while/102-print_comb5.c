1dbfbe84c64784daa0889ca87cc64cbf6b46f290
Tolulope <debbyonasanya@gmail.com>
2023-03-22 08:59:49 -0700

    combinations2

--- 0x01-variables_if_else_while/102-print_comb5.c
+++ 0x01-variables_if_else_while/102-print_comb5.c
@@ -12,21 +12,27 @@ int main(void)
 	int k;
 	int l;
 
-	for (i = 0; i <= 10; i++)
+	for (i = 0; i < 10; i++)
 	{
-		for (j = 0; j <= 10; j++)
+		for (j = 0; j < 10; j++)
 		{
-			for (k = 0; k <= 10; k++)
+			for (k = 0; k < 10; k++)
 			{
-				for (l = 1; l <= 10; l++)
+				for (l = 1; l < 10; l++)
 				{
-					putchar(i + '0');
-					putchar(j + '0');
-					putchar(' ');
-					putchar(k + '0');
-					putchar(l + '0');
-					putchar(',');
-					putchar(' ');
+					if (i + j < k + l)
+					{
+						putchar(i + '0');
+						putchar(j + '0');
+						putchar(' ');
+						putchar(k + '0');
+						putchar(l + '0');
+						if (i + j != 17 && j + k != 18)
+						{
+							putchar(',');
+							putchar(' ');
+						}
+					}
 				}
 			}
 		}
