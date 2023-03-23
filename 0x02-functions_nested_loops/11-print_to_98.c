4326fff26baa2140399f9d8d355e779ed75707e1
Tolulope <debbyonasanya@gmail.com>
2023-03-22 21:08:31 -0700

    print to 98

---
+++ 0x02-functions_nested_loops/11-print_to_98.c
@@ -0,0 +1,56 @@
+#include "main.h"
+/**
+ * print_to_98 - function to print natural numbers
+ *
+ * @n: parameter to be checked
+ * Return: always 0
+ */
+void print_to_98(int n)
+{
+	int i;
+
+	if (n >= 0 && n <= 98)
+	{
+		for (i = n; i <= 98; i++)
+		{
+			_putchar(i + '0');
+			if (i != 98)
+			{
+				_putchar(',');
+				_putchar(' ');
+			}
+		}
+		_putchar('\n');
+	}
+	else if (n < 0)
+	{
+		for (i = n; i <= 98; i++)
+		{
+			_putchar(i + '0');
+			if (i != 98)
+			{
+				_putchar(',');
+				_putchar(' ');
+			}
+		}
+		_putchar('\n');
+	}
+	else if (n > 98)
+	{
+		for (i = n; i >= 98; i--)
+		{
+			_putchar(i + '0');
+			if (i != 98)
+			{
+				_putchar(',');
+				_putchar(' ');
+			}
+		}
+		_putchar('\n');
+	}
+	else if (n == 98)
+	{
+		_putchar(n + '0');
+	}
+}
+
