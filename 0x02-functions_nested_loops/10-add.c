476e3cd018f03cc749a8e6c1e3a9ccccbf4dae9b
Tolulope <debbyonasanya@gmail.com>
2023-03-22 20:43:46 -0700

    add

---
+++ 0x02-functions_nested_loops/10-add.c
@@ -0,0 +1,11 @@
+#include "main.h"
+/**
+ * add - function for adding two integers
+ * @n: parameter to be chcecked
+ * @m: parameter to be checked
+ * Return: return a + b
+ */
+int add(int m, int n)
+{
+	return (m + n);
+}
