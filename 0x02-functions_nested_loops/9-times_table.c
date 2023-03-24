e107f55e11895aecfd23c004212c75573f80164c
Tolulope <debbyonasanya@gmail.com>
2023-03-24 10:33:45 -0700

    times_table

---
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -0,0 +1,26 @@
+#include "main.h"
+
+/**
+ * times_table - function to print times table 
+ * Return: always 0
+ */
+
+void times_table(void)
+{
+	int i;
+	int j;
+
+	for (i = 0; i <= 9; i++)
+	{
+		for (j = 0; j <= 9; j++)
+		{
+			_putchar(i * j + '0');
+			if (j < 9)
+			{
+				_putchar(',');
+				_putchar(' ');
+			}
+		}
+		_putchar('\n');
+	}
+}
--- 0x02-functions_nested_loops/main.h
+++ 0x02-functions_nested_loops/main.h
@@ -12,6 +12,7 @@ int print_last_digit(int);
 int add(int, int);
 void jack_bauer(void);
 void print_to_98(int n);
+void times_table(void);
 
 
 #endif
