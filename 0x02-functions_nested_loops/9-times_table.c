19f545f68d0bdd29f51d49a60405f1ee3d3b763f
Tolulope <debbyonasanya@gmail.com>
2023-03-24 12:56:22 -0700

    times_table

--- 0x02-functions_nested_loops/9-times_table.c
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -14,21 +14,28 @@ void times_table(void)
 	{
 		for (j = 0; j <= 9; j++)
 		{
-			if (i * j > 9)
+			if (i * j < 10)
 			{
-				_putchar(i * j / 10 + '0');
-				_putchar(i * j % 10 + '0');
+				_putchar(i * j + '0');
+				if (j < 9)
+				{
+					_putchar(',');
+					_putchar(' ');
+					_putchar(' ');
+				}
 			}
 			else
 			{
-				_putchar(i * j + '0');
-			}
-			if (j < 9)
-			{
-				_putchar(',');
-				_putchar(' ');
+				_putchar(i * j / 10 + '0');
+				_putchar(i * j % 10 + '0');
+				if (j < 9)
+				{
+
+					_putchar(',');
+					_putchar(' ');
+				}
 			}
 		}
-	_putchar('\n');
+		_putchar('\n');
 	}
 }
