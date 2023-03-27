76a5b37b2d6d58dc95e967474839c4bd5c0c23a0
Tolulope <debbyonasanya@gmail.com>
2023-03-26 18:16:26 -0700

    Fibonacci

--- 0x02-functions_nested_loops/102-fibonacci.c
+++ 0x02-functions_nested_loops/102-fibonacci.c
@@ -9,15 +9,15 @@ int main(void)
 	int t1 = 1;
 	int t2 = 2;
 	int nextTerm = t1 + t2;
-	
+
 	printf("%d, %d, ", t1, t2);
 	for (i = 3; i <= 50; ++i)
-      	{
+	{
 		printf("%d, ", nextTerm);
-	        t1 = t2;
-	        t2 = nextTerm;
-	        nextTerm = t1 + t2;
-	 }
+		t1 = t2;
+		t2 = nextTerm;
+		nextTerm = t1 + t2;
+	}
 	printf("\n");
-	 return 0;
+	return (0);
 }
