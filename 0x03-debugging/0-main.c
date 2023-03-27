63488bd0e12e2cb7b179db41d25ed6fdb5e24ef7
Tolulope <debbyonasanya@gmail.com>
2023-03-27 08:17:51 -0700

    error checking

--- 0x03-debugging/0-main.c
+++ 0x03-debugging/0-main.c
@@ -1,20 +1,16 @@
 #include "main.h"
+
 /**
- * positive_or_negative - function to check for positive or negative number
- *
- * @i: parameter to be checked
- *
- * Return: always 0
+ * main - tests function that prints if integer is positive or negative
+ * Return: 0
  */
 
-void positive_or_negative(int i)
+int main(void)
 {
-	if (i < 0)
-	{
-		printf("%d is negative", i);
-	}
-	else
-		printf("%d is positive", i);
-	printf("\n");
-}
+	int i;
 
+	i = 0;
+	positive_or_negative(i);
+
+	return (0);
+}
