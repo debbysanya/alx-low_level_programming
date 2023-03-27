b07e3323eda5b31fb9cfbe3496cb10093b3cfb97
Tolulope <debbyonasanya@gmail.com>
2023-03-27 13:20:46 -0700

    Multiple

---
+++ 0x04-more_functions_nested_loops/2-mul.c
@@ -0,0 +1,19 @@
+#include "main.h"
+/**
+ * mul - function to multiply two integers
+ *
+ * @a: parameter to be checked
+ * @b: parameter to be checked
+ *
+ * Return: return multiple
+ */
+
+int mul(int a, int b)
+{
+	int multiple;
+
+	multiple = a * b;
+
+	return (multiple);
+	_putchar('\n');
+}
