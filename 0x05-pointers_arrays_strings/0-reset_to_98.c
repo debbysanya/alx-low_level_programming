0286d20cb0cc33554212d3c317b3dcf045139108
Tolulope <debbyonasanya@gmail.com>
2023-03-28 08:07:24 -0700

    Pointers and arrays

---
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -0,0 +1,12 @@
+#include "main.h"
+/**
+ * reset_to_98 - function to update value of a pointer
+ * @n: parameter to be checked
+ * Return: always 0
+ */
+void reset_to_98(int *n)
+{
+	*n = 98;
+
+	_putchar(*n + '0');
+}
---
+++ 0x05-pointers_arrays_strings/README.md
@@ -0,0 +1,1 @@
+# C - Arrays, Strings and Pointers
