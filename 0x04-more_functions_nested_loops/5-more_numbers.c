0c572aebbad752a3e4e01720190f7780d7623f30
Tolulope <debbyonasanya@gmail.com>
2023-03-29 08:19:10 -0700

    more numbers

--- 0x04-more_functions_nested_loops/5-more_numbers.c
+++ 0x04-more_functions_nested_loops/5-more_numbers.c
@@ -11,13 +11,13 @@ void more_numbers(void)
 
 	for (i = 0; i < 10; i++)
 	{
-		for (j = 0; j <= 14; j++)
+		for (j = 0; j < 15; j++)
 		{
-			_putchar(j / 1 + '0');
-			if (j > 9)
+			if (j >= 10)
 			{
-				_putchar(j % 10 + '0');
+				_putchar((j / 10) + 48);
 			}
+			_putchar((j % 10) + 48);
 		}
 		_putchar('\n');
 	}
