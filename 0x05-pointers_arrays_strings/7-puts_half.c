6155ffc07916a31d1cb07f03ffc6a516f1cecb9e
Tolulope <debbyonasanya@gmail.com>
2023-03-28 19:51:53 -0700

    half of string

---
+++ 0x05-pointers_arrays_strings/7-puts_half.c
@@ -0,0 +1,26 @@
+#include "main.h"
+/**
+ * puts_half - function to print half of string
+ * @str: parameter
+ * Return: always 0
+ */
+void puts_half(char *str)
+{
+	int i;
+	int count = 0;
+
+	for (i = 0; str[i] != '\0'; i++)
+		count++;
+	if (count % 2 == 0)
+	{
+		for (i = count / 2; str[i] != '\0'; i++)
+			_putchar(str[i]);
+		_putchar('\n');
+	}
+	else if (count % 2 != 0)
+	{
+		for (i = (count - 1) / 2; str[i] != '\0'; i++)
+			_putchar(str[i]);
+		_putchar('\n');
+	}
+}
