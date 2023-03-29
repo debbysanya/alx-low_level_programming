65c74dac3cf7ccfe909ada80252c2a23f9e8495f
Tolulope <debbyonasanya@gmail.com>
2023-03-28 19:14:41 -0700

    reverse

--- 0x05-pointers_arrays_strings/4-print_rev.c
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -6,13 +6,12 @@
  */
 void print_rev(char *s)
 {
-	int i;
-	int j;
-	int count = 0;
+	int i = 0;
 
-	for (i = 0; s[i] != '\0'; s++)
-		count++;
-	for (j = count; j >= 0; j--)
-		_putchar(s[j]);
+	while (s[i] != '\0')
+		i++;
+	for (i = i - 1; i >= 0; i--)
+		_putchar(s[i]);
+
 	_putchar('\n');
 }
