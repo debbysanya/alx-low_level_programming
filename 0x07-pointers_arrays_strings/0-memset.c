bec93a889384e4859e771f5cb8b4560b77f7305d
Tolulope <debbyonasanya@gmail.com>
2023-04-03 19:48:09 -0700

    Constant byte

---
+++ 0x07-pointers_arrays_strings/0-memset.c
@@ -0,0 +1,18 @@
+#include "main.h"
+/**
+ * _memset - function to fill memory with a constant byte
+ * @s: parameter
+ * @b: parameter
+ * @n: parameter
+ * Return: char
+ */
+char *_memset(char *s, char b, unsigned int n)
+{
+	int i = 0;
+	for (; n > 0; i++)
+	{
+		s[i] = b;
+		n--;
+	}
+	return (s);
+}
