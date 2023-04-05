0b9e7d2bb8e91b76441a5ad929b78d968a074db8
Tolulope <debbyonasanya@gmail.com>
2023-04-05 14:44:30 -0700

    printing a string

---
+++ 0x08-recursion/0-puts_recursion.c
@@ -0,0 +1,16 @@
+#include "main.h"
+/**
+ * _puts_recursion - function to print a string
+ * @s: parameter
+ * Return: always 0;
+ */
+void _puts_recursion(char *s)
+{
+	if (*s)
+	{
+		_putchar(*s);
+		_puts_recursion(s + 1);
+	}
+	else
+		_putchar('\n');
+}
