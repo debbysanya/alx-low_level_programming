722407d402e02f42db8a28026b507d4b0ee7274f
Tolulope <debbyonasanya@gmail.com>
2023-04-10 19:44:05 -0700

    change

--- 0x0A-argc_argv/100-change.c
+++ 0x0A-argc_argv/100-change.c
@@ -1,4 +1,5 @@
 #include "main.h"
+#include <stdio.h>
 /**
  * main - function
  * @argc: argument count
@@ -11,13 +12,13 @@ int main(int argc, char *argv[])
 
 	int coins[] = {25, 20, 5, 2, 1};
 
-	if (argc != j)
+	if (argc != 2)
 	{
-		printf("Erroe\n");
-		return (0);
+		printf("Error\n");
+		return (1);
 	}
-	num = _atoi(argv[1])
-		result = 0;
+	num = _atoi(argv[1]);
+	result = 0;
 	if (num < 0)
 	{
 		printf("0\n");
