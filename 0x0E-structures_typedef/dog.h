a5addc65f6e0b691cc4e334b6f4924df9268f078
Tolulope <debbyonasanya@gmail.com>
2023-04-18 17:03:59 -0700

    defining a new type

--- 0x0E-structures_typedef/dog.h
+++ 0x0E-structures_typedef/dog.h
@@ -9,12 +9,16 @@
  * Description: This is a new type that has the following elements
  * name, age and owner
  */
-typedef struct dog
+struct dog
 {
 	char *name;
 	float age;
 	char *owner;
-} dog;
+};
+/**
+ * gog_t - a new type as a new name for struct dog
+ */
+typedef struct dog dog_t;
 void init_dog(struct dog *d, char *name, float age, char *owner);
 void print_dog(struct dog *d);
 #endif
