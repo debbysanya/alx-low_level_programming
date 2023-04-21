bd9489bda5357c3de112235ee2694c74f912c486
Tolulope <debbyonasanya@gmail.com>
2023-04-20 19:40:37 -0700

    print strings

--- 0x10-variadic_functions/1-print_numbers.c
+++ 0x10-variadic_functions/1-print_numbers.c
@@ -12,14 +12,15 @@ void print_numbers(const char *separator, const unsigned int n, ...)
 {
 	va_list ap;
 	unsigned int i, s;
- 
+
 	va_start(ap, n);
 	for (i = 0; i < n; i++)
 	{
 		s = va_arg(ap, int);
-		printf("%d",s);
+
+		printf("%d", s);
 		if (i != (n - 1) && separator != NULL)
-			printf("%s",separator);
+			printf("%s", separator);
 	}
 	va_end(ap);
 	printf("\n");
---
+++ 0x10-variadic_functions/2-print_strings.c
@@ -0,0 +1,32 @@
+#include <stdio.h>
+#include <stdarg.h>
+#include <stdlib.h>
+/**
+ * print_strings - function that prints strings, followed by a new line
+ * @*separator: string comma
+ * @n: number of strings
+ * Return: always 0
+ */
+
+void print_strings(const char *separator, const unsigned int n, ...)
+{
+	va_list ap;
+	unsigned int i;
+	char *str;
+
+	va_start(ap, n);
+	for (i = 0; i < n; i++)
+	{
+		str = va_arg(ap, char *);
+		if (str == NULL)
+			printf("nil");
+		else
+			printf("%s", str);
+
+		if (i != (n - 1) && (separator != NULL))
+			printf("%s", separator);
+
+	}
+	printf("\n");
+	va_end(ap);
+}
