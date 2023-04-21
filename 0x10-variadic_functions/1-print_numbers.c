0cfd4c0ead6eb30c0195c6acf592a42b46087553
Tolulope <debbyonasanya@gmail.com>
2023-04-20 20:00:44 -0700

    print numbers

--- 0x10-variadic_functions/1-print_numbers.c
+++ 0x10-variadic_functions/1-print_numbers.c
@@ -19,7 +19,7 @@ void print_numbers(const char *separator, const unsigned int n, ...)
 		s = va_arg(ap, int);
 
 		printf("%d", s);
-		if (i != (n - 1) && separator != "NULL")
+		if (i != (n - 1) && separator != NULL)
 			printf("%s", separator);
 	}
 	va_end(ap);
