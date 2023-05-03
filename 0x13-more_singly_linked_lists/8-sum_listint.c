4e2a845406aeac9c123a7f712c47529b9cac10a4
Tolulope <debbyonasanya@gmail.com>
2023-05-02 17:57:47 -0700

    returns nth node of a list

---
+++ 0x13-more_singly_linked_lists/8-sum_listint.c
@@ -0,0 +1,20 @@
+#include "lists.h"
+
+/**
+ * sum_listint - function that calculates the sum
+ * of all the data in a listint_t list
+ * @head: first node in the linked list
+ * Return: resulting sum
+ */
+int sum_listint(listint_t *head)
+{
+	int add = 0;
+	listint_t *temp = head;
+
+	while (temp)
+	{
+		add += temp->n;
+		temp = temp->next;
+	}
+	return (add);
+}
