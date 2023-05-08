cc79c74e7f085e2ed8a3e6f36dcdc2925c1cabd0
Tolulope <debbyonasanya@gmail.com>
2023-05-07 20:23:10 -0700

    binary representation of a number

--- 0x14-bit_manipulation/1-print_binary.c
+++ 0x14-bit_manipulation/1-print_binary.c
@@ -5,4 +5,14 @@
  * @n: unsigned long integer
  * Return: always 0
  */
-
+void print_binary(unsigned long int n)
+{
+	if (n >> 0)
+	{
+		if (n >> 1)
+			print_binary(n >> 1);
+		_putchar((n & 1) + '0');
+	}
+	else
+		_putchar('0');
+}
